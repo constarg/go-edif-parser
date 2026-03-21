3e8b955da5d0d88278c7d443b8e41c37187e88fb
Constantinos Argyriou <argyrioukost@gmail.com>
2026-03-21 15:09:40 +0200

    Implement functions for removing/adding netlist elements

--- pkg/edif/ediflist_addelement.go
+++ pkg/edif/ediflist_addelement.go
@@ -1,13 +1,70 @@
+// File: ediflist_addelement.go
+//
+// **********************************************************************
+//
+// Implements the add element function, which allows to either push or insert
+// a new element in a netlist.
+//
+// Copyright (C) 2026  Constantinos Argyriou
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Email: constarg@pm.me
+// ***********************************************************************
+
+// Package edif Contains various structures, and functions related to the
+// modification and parsing of EDIF (Electronic Design Interchange Format) files.
+// This can be used for example to manipulate the netlist of a circuit which
+// is produced by EDA tools, like Vivado.
 package edif
 
+import (
+	"container/list"
+	"errors"
+)
+
 func (edifList *List) InsertElement(
-	element ListElement, after ListElement,
-) (error, *List) {
-	// TODO: Implement me.
-	return nil, nil
+	element *ListElement, after *ListElement,
+) error {
+	// Holds the element of the list, after which the new element should
+	// be inserted.
+	var markElement *list.Element
+
+	if edifList.children.Len() == 0 {
+		return errors.New(
+			"edif: no elements in the list",
+		)
+	}
+
+	markElement = nil
+	for curr := edifList.children.Front(); curr != nil; curr = curr.Next() {
+		if curr.Value == after {
+			markElement = curr
+		}
+	}
+
+	if markElement == nil {
+		return errors.New(
+			"edif: cannot insert element, no element 'after'" +
+				"found",
+		)
+	}
+
+	edifList.children.InsertAfter(element, markElement)
+	return nil
 }
 
-func (edifList *List) PushElement(element ListElement) (error, *List) {
-	// TODO: Implement me.
-	return nil, nil
+func (edifList *List) PushElement(element *ListElement) {
+	edifList.children.PushBack(element)
 }
--- pkg/edif/ediflist_removeelement.go
+++ pkg/edif/ediflist_removeelement.go
@@ -1,1 +1,53 @@
+// File: ediflist_addelement.go
+//
+// **********************************************************************
+//
+// Implements the remove element function, which allows to remove an elemnent
+// from a netlist.
+//
+// Copyright (C) 2026  Constantinos Argyriou
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Email: constarg@pm.me
+// ***********************************************************************
+
+// Package edif Contains various structures, and functions related to the
+// modification and parsing of EDIF (Electronic Design Interchange Format) files.
+// This can be used for example to manipulate the netlist of a circuit which
+// is produced by EDA tools, like Vivado.
 package edif
+
+import (
+	"container/list"
+	"errors"
+)
+
+func (edifList *List) RemoveElement(element *ListElement) error {
+	// Holds the element to be removed.
+	var elementToRemove *list.Element
+
+	for curr := edifList.children.Front(); curr != nil; curr = curr.Next() {
+		if curr.Value == element {
+			elementToRemove = curr
+			break
+		}
+	}
+	if elementToRemove == nil {
+		return errors.New("edif: element not found")
+	}
+
+	edifList.children.Remove(elementToRemove)
+	return nil
+}
